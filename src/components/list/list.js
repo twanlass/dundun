9bd55b7b781a29247d155aeef641b98a0ce44736
Tyler Wanlass <tyler@buffer.com>
2017-11-26 07:04:21 -0800

    Refactor listSection back into list component.

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import ListSection from '../../components/listSection/listSection.js';
+import _ from 'lodash';
+import classNames from 'classnames';
+import { CSSTransitionGroup } from 'react-transition-group';
+import ListSectionHeader from '../listSectionHeader/listSectionHeader.js'
+import ListItem from '../listItem/listItem.js';
+import ListItemForm from '../listItemForm/listItemForm.js';
+import './list.css';
 
 export default class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showDone: true
+    };
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.activeList !== this.props.activeList) {
       this.props.getListItems(nextProps.activeList)
     }
+  }
+
+  toggleShowDone() {
+    let showDone = !this.state.showDone;
+    this.setState({ showDone: showDone });
+  }
+
+  renderListSectionHeader(title) {
+    const {items, activeList}  = this.props;
+    let badgeCount = _.filter(items, { 'completed': false, 'list_id': activeList}).length;
+
+    return (
+      <ListSectionHeader
+        showDone={this.state.showDone}
+        toggleShowDone={this.toggleShowDone.bind(this)}
+        title={title}
+        badgeCount={badgeCount}
+      />
+    )
+  }
+
+  // If a temp client id (cid) exists, use that as the key to
+  // avoid rendering an item twice (temp item, and real API returned item)
+  itemKey(item) {
+    return item.cid ? item.cid : item.id;
   }
 
   handleDragStart(e) {
@@ -78,33 +117,86 @@ export default class List extends React.Component {
   }
 
   render() {
-    const {activeList} = this.props;
+    const {items, sorts, nowEditing, nowDragging, add, remove, done, reorder, edit, onEdit, activeList}  = this.props;
 
-    if (activeList) {
+    let itemOrder = sorts[activeList];
+    let completedItems = []
+    let notCompletedItems = []
+    let allItems = []
+    let lastItemId = null
+
+    if (itemOrder) {
+      itemOrder.forEach(function(item) {
+        if (items[item]) {
+          items[item].completed ? completedItems.push(items[item]) : notCompletedItems.push(items[item]);
+        }
+      })
+
+      lastItemId = notCompletedItems.length ? notCompletedItems[notCompletedItems.length - 1].id : null;
+
+      if(this.state.showDone) {
+        allItems = _.union(notCompletedItems, completedItems);
+      } else {
+        allItems = notCompletedItems;
+      }
+    }
+
+    let itemsClasses = classNames(
+      'todos',
+      {'todos--drag-active': nowDragging}
+    );
+
+    let itemSectionClasses = classNames(
+      'todo-list'
+    )
+
+    if (allItems.length) {
       return (
-        <ListSection
-          key='today'
-          title='today'
-          items={this.props.items}
-          sorts={this.props.sorts[activeList]}
-          add={this.props.add.bind(this)}
-          edit={this.props.edit.bind(this)}
-          done={this.props.done.bind(this)}
-          reorder={this.props.reorder.bind(this)}
-          remove={this.props.remove.bind(this)}
-          onEdit={this.props.onEdit.bind(this)}
-          dragStart={this.handleDragStart.bind(this)}
-          dragEnd={this.handleDragEnd.bind(this)}
-          dragOver={this.handleDragOver.bind(this)}
-          nowEditing={this.props.nowEditing}
-          nowDragging={this.props.nowDragging}
-          nowDraggingFrom={this.props.nowDragging}
-          nowDraggingTo={this.props.nowDragging}
-          activeList={this.props.activeList}
-        />
+        <div className={itemSectionClasses}>
+          {this.renderListSectionHeader('today')}
+          <CSSTransitionGroup
+            transitionName="todo-"
+            component="div"
+            className={itemsClasses}
+            data-id={this.props.title}
+            onDragOver={this.handleDragOver.bind(this)}
+            transitionEnterTimeout={250}
+            transitionLeaveTimeout={150}>
+              {allItems.map(item => (
+                <ListItem
+                  todo={item}
+                  key={this.itemKey(item)}
+                  nowEditing={nowEditing}
+                  remove={remove}
+                  done={done}
+                  reorder={reorder}
+                  edit={edit}
+                  onEdit={onEdit}
+                  dragStart={this.handleDragStart.bind(this)}
+                  dragEnd={this.handleDragEnd.bind(this)}
+                  activeList={activeList}
+                />
+              ))}
+          </CSSTransitionGroup>
+          <ListItemForm
+            add={add}
+            onEdit={onEdit}
+            nowEditing={nowEditing}
+            lastItemId={lastItemId}
+            activeList={activeList}
+          />
+        </div>
       )
     } else {
-      return null;
+      return (
+        <ListItemForm
+          add={add}
+          onEdit={onEdit}
+          nowEditing={nowEditing}
+          lastItemId={lastItemId}
+          activeList={activeList}
+        />
+      )
     }
   }
 }
